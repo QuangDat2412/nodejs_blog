d18ad9e39d71140cbe4ec42f41db311ca0d4fb04
QuangDat2412 <ledatumltk123@gmail.com>
2021-09-26 02:22:57 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/dat_dev', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect('mongodb://localhost:27017/dat_dev', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('Connect successfully');
     } catch (e) {
         console.log('Connect Failure');
